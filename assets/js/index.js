1f272e3bcd28c9b2493e0979366e353f2fe021f2
TusharKadam <tushar.kadam.n@gmail.com>
2024-12-14 02:07:50 +0530

    Added index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,11 @@
-// Functionality of the page should appear here
+// Functionality of the page should appear here// Functionality of the page should appear here
+let accButton2 = document.querySelectorAll(".acc-listItem");
+const listDesc = document.querySelectorAll(".acc-listDesc");
+const dropDown = document.querySelectorAll(".acc-dropdown");
+
+accButton2.forEach((button) => {
+  button.addEventListener("click", function () {
+    button.querySelector(".acc-listDesc").classList.toggle("show");
+    button.querySelector(".acc-dropdown").classList.toggle("acc-dropdownup");
+  });
+});
